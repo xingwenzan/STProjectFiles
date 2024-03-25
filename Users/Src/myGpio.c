b869dd1c9915b48c66f1ff6e1b307a25b01e3b13
86159 <zxw3180716935@163.com>
2024-03-26 01:13:48 +0800

    add some note

--- Users/Src/myGpio.c
+++ Users/Src/myGpio.c
@@ -8,9 +8,9 @@ void MY_GPIO_INIT(void) {
     GPIO_InitTypeDef GPIO_InitStruct = {0};
 
     /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOH_CLK_ENABLE();
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
+//    __HAL_RCC_GPIOH_CLK_ENABLE();   // 意义不明，不影响 LED 闪烁
+    __HAL_RCC_GPIOC_CLK_ENABLE();   // C端口（LED）时钟使能，没有不亮
+//    __HAL_RCC_GPIOA_CLK_ENABLE();   // 意义不明，不影响 LED 闪烁
 
     /*Configure GPIO pin Output Level */
     HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
